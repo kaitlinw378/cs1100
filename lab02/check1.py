4dc38e02263356618d229fb0a7acd5bf3b0994c7
kaitlinw378 <93414927+kaitlinw378@users.noreply.github.com>
2022-05-11 18:19:56 -0700

    Add files via upload

---
+++ lab02/check1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 22:29:37 2019
+
+@author: kaitlin
+"""
+
+print("*"*10)
+print("*"*2, "spam", "*"*2)
+print("*"*10)
---
+++ lab02/check2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 22:38:40 2019
+
+@author: kaitlin
+"""
+
+word = input("Enter a four letter word: ")
+print("*"*10)
+print("*"*2, word, "*"*2)
+print("*"*10)
